aa936c2fbf8aad57fe8de4b60ad1e66cbb805d00
yx597249755 <yx597249755@163.com>
2019-06-22 08:36:14 +0800

    初始化项目

---
+++ src/main/java/com/lei/concurrentdemo/ConcurrentdemoApplication.java
@@ -0,0 +1,13 @@
+package com.lei.concurrentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ConcurrentdemoApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/lei/concurrentdemo/ConcurrentdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lei.concurrentdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrentdemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
